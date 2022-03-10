2f672c3f39b70d2ddafe513a0828397156335816
Vincent Serpoul <vincent@serpoul.com>
2022-03-11 00:56:07 +0800

    rename

--- complex/rest2stream/benchmarks/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -eou pipefail
-
-k6 run -d 2s -u 30 benchmarks/k6.js
--- complex/rest2stream/pkg/word/msg.go
+++
@@ -1,8 +0,0 @@
-package word
-
-import "go.opentelemetry.io/otel/trace"
-
-type WordMsg struct {
-	SpanContext trace.SpanContext `json:"span_context"`
-	Word        string            `json:"word"`
-}
