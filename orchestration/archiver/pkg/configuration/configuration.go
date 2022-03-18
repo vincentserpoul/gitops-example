05ce070ec3c5e78dd11bad6025835fbd46fb4e5e
Vincent Serpoul <vincent@serpoul.com>
2022-03-18 18:29:41 +0800

    fix(config): duplicate entry

--- orchestration/archiver/pkg/configuration/configuration.go
+++ orchestration/archiver/pkg/configuration/configuration.go
@@ -8,16 +8,19 @@ import (
 
 type MissingEnvConfigError struct {
 	env string
+	err error
 }
 
 func (mece MissingEnvConfigError) Error() string {
-	return fmt.Sprintf("missing config %s", mece.env)
+	return fmt.Sprintf("missing config %s: %v", mece.env, mece.err)
 }
 
-type MissingBaseConfigError struct{}
+type MissingBaseConfigError struct {
+	err error
+}
 
 func (mbce MissingBaseConfigError) Error() string {
-	return "missing base config"
+	return fmt.Sprintf("missing base config: %v", mbce.err)
 }
 
 func GetConfig(currEnv string) (*Config, error) {
@@ -25,11 +28,11 @@ func GetConfig(currEnv string) (*Config, error) {
 	var cfg Config
 
 	if err := cleanenv.ReadConfig("./config/base.yaml", &cfg); err != nil {
-		return nil, MissingBaseConfigError{}
+		return nil, MissingBaseConfigError{err: err}
 	}
 
 	if err := cleanenv.ReadConfig(fmt.Sprintf("./config/%s.yaml", currEnv), &cfg); err != nil {
-		return &cfg, MissingEnvConfigError{env: currEnv}
+		return &cfg, MissingEnvConfigError{env: currEnv, err: err}
 	}
 
 	return &cfg, nil
