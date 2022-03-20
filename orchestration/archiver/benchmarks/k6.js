e73305fdd814cd1a2c179b8287717439803f6ff7
Vincent Serpoul <vincent@serpoul.com>
2022-03-20 20:47:52 +0800

    test(bench): k6 bench update

--- orchestration/archiver/benchmarks/k6.js
+++ orchestration/archiver/benchmarks/k6.js
@@ -1,4 +1,5 @@
 import http from "k6/http";
+import { uuidv4 } from "https://jslib.k6.io/k6-utils/1.1.0/index.js";
 import { check } from "k6";
 import { Rate } from "k6/metrics";
 
@@ -7,15 +8,28 @@ export let errorRate = new Rate("errors");
 export default function () {
   const environment = `${__ENV.APP_ENVIRONMENT}`;
 
-
-  var url = "http://localhost:3001/word";
+  var url = "http://localhost:3001/v1/happycatfact";
   if(environment == "dev"){
-    url = "https://archiver.orchestration.dev:8443/word";
+    url = "https://archiver.orchestration.dev:8443/v1/happycatfact";
   }
 
-  var params = {};
+  var params = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-  check(http.post(url, "bench", params), {
-    "status is 201": (r) => r.status == 201,
-  }) || errorRate.add(1);
+  var payload = JSON.stringify(
+    {
+      id: uuidv4(),
+      fact: "bench"
+    }
+  )
+
+  check(
+    http.post(url, payload, params),
+    {
+      "status is 201": (r) => r.status == 201,
+    }
+  ) || errorRate.add(1);
 }
--- orchestration/archiver/benchmarks/run.sh
+++ orchestration/archiver/benchmarks/run.sh
@@ -2,4 +2,4 @@
 
 set -eou pipefail
 
-k6 run -d 2s -u 30 benchmarks/k6.js
+k6 run -d 5s -u 30 benchmarks/k6.js --no-usage-report
