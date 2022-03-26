fe8c482a188fc035b2b4c80e9753e43bc5e81070
Vincent Serpoul <vincent@serpoul.com>
2022-03-26 17:05:57 +0800

    refactor: remove read/write users

--- orchestration/archiver/sql/init/init.sql
+++ orchestration/archiver/sql/init/init.sql
@@ -1,14 +1,8 @@
 -- kubectl port-forward svc/mypostgres 5436:5432 -n postgres
 -- make init
 -- user creation
-CREATE USER archiver_write WITH PASSWORD 'DB_PASSWORD';
-
-CREATE USER archiver_read WITH PASSWORD 'DB_PASSWORD';
+CREATE USER archiver_app WITH PASSWORD 'DB_PASSWORD';
 
 CREATE DATABASE archiverDB_SUFFIX;
 
-GRANT ALL PRIVILEGES ON DATABASE archiverDB_SUFFIX TO archiver_write;
-
-GRANT CONNECT ON DATABASE archiverDB_SUFFIX TO archiver_read;
-
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO archiver_read;
+GRANT ALL PRIVILEGES ON DATABASE archiverDB_SUFFIX TO archiver_app;
