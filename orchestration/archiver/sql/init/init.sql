a21476c6dbc1d01164c66476f4c1ef2c074e8f98
Vincent Serpoul <vincent@serpoul.com>
2022-03-29 11:12:02 +0800

    fix: better init sql

--- orchestration/archiver/sql/init/init.sql
+++ orchestration/archiver/sql/init/init.sql
@@ -1,8 +1,8 @@
 -- kubectl port-forward svc/mypostgres 5436:5432 -n postgres
 -- make init
 -- user creation
-CREATE USER archiver_app WITH PASSWORD 'DB_PASSWORD';
+CREATE USER IF NOT EXISTS archiver_app WITH PASSWORD 'DB_PASSWORD';
 
-CREATE DATABASE archiverDB_SUFFIX;
+CREATE DATABASE IF NOT EXISTS  archiverDB_SUFFIX;
 
 GRANT ALL PRIVILEGES ON DATABASE archiverDB_SUFFIX TO archiver_app;
