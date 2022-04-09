bc654baaa906aa124f22f097e2755c1e81c8caa1
Vincent Serpoul <vincent@serpoul.com>
2022-04-09 15:16:00 +0800

    style: rename shortcut for storage

--- simple/gohttp/pkg/user/storage.go
+++ simple/gohttp/pkg/user/storage.go
@@ -22,8 +22,8 @@ type Storage struct {
 	sync.Map
 }
 
-func (us *Storage) Get(id uuid.UUID) (*User, error) {
-	if userI, ok := us.Load(id); ok {
+func (st *Storage) Get(id uuid.UUID) (*User, error) {
+	if userI, ok := st.Load(id); ok {
 		if user, ok := userI.(*User); ok {
 			return user, nil
 		}
@@ -32,10 +32,10 @@ func (us *Storage) Get(id uuid.UUID) (*User, error) {
 	return nil, NoUserFoundError{}
 }
 
-func (us *Storage) Create(user *User) {
-	us.Store(user.ID, user)
+func (st *Storage) Create(user *User) {
+	st.Store(user.ID, user)
 }
 
-func (us *Storage) Delete(id uuid.UUID) error {
-	return us.Delete(id)
+func (st *Storage) Delete(id uuid.UUID) error {
+	return st.Delete(id)
 }
